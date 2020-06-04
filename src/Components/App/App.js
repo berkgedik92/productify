ecfeca4f6aaba5adda0d296e906c95efbfee0c46
Berk Gedik <berk.gedik@tink.se>
2020-06-04 14:15:02 +0200

    Fix for requestHandler error case
    This is the problem that was causing the bug:
    
    In case there is an error while fetching data (just intentionally use a wrong URL to reproduce the issue locally, for example instead of "/tasks" use "/taskssss"), the error is caught in the fetchData method of requestHandler but App.js -which is calling fetchData- tries to apply a then block after catch. Since the catch block does not provide any value for the chain to continue, then block cannot be executed.
    
    The solution I implemented is, I just moved to catch block from requestHandler.js to App.js to make it executed after all "then" blocks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,25 @@
-  import React, { Component } from 'react';
-  import Header from '../Header/Header';
-  import MainContentRouter from '../MainContentRouter/MainContentRouter';
-  import ProductifyContext from '../../ProductifyContext';
-  import { fetchData, updateTaskInDb, deleteTaskInDb, addNewUser } from '../../requestHandler';
-  import CONFIG from '../../config';
-  import './App.css';
+import React, {Component} from 'react';
+import Header from '../Header/Header';
+import MainContentRouter from '../MainContentRouter/MainContentRouter';
+import ProductifyContext from '../../ProductifyContext';
+import {fetchData, updateTaskInDb, deleteTaskInDb, addNewUser} from '../../requestHandler';
+import CONFIG from '../../config';
+import './App.css';
 
-  export default class App extends Component {
+export default class App extends Component {
 
-    constructor(props){
-      super(props);
-      this.state = {
-        loggedInUser: null, 
-        usersInfo: [],
-        usersLogin: [],
-        teams: [],
-        projects: [],
-        tasks: [],
-        currentProject: null,
-        showAllUserTasks: true,
-      }
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedInUser: null,
+            usersInfo: [],
+            usersLogin: [],
+            teams: [],
+            projects: [],
+            tasks: [],
+            currentProject: null,
+            showAllUserTasks: true,
+        }
     }
 
     componentDidMount(){
@@ -27,117 +27,117 @@
       fetchData()
         .then(([teams, projects, usersInfo, usersLogin, tasks]) => {
           this.setState({teams, projects, usersInfo, usersLogin, tasks, loggedInUser})
-        })
+        }).catch(e => console.log(e));
     }
 
     logInUser = (userToLogIn) => {
-      this.setState({
-        ...this.state,
-        loggedInUser: userToLogIn.user_id
-      })
+        this.setState({
+            ...this.state,
+            loggedInUser: userToLogIn.user_id
+        })
     }
 
     signOutUser = () => {
-      this.setState({
-        ...this.state,
-        loggedInUser: null
-      })
+        this.setState({
+            ...this.state,
+            loggedInUser: null
+        })
     }
 
     signUpUser = (newUserLogin, newUserInfo) => {
-      const { id } = newUserInfo;
-      // Simultaneously add new user and log user in
-      addNewUser(newUserInfo, newUserLogin);
-      this.setState({
-        ...this.state,
-        usersLogin: [...this.state.usersLogin, newUserLogin],
-        usersInfo: [...this.state.usersInfo, newUserInfo],
-        loggedInUser: id
-      })
+        const {id} = newUserInfo;
+        // Simultaneously add new user and log user in
+        addNewUser(newUserInfo, newUserLogin);
+        this.setState({
+            ...this.state,
+            usersLogin: [...this.state.usersLogin, newUserLogin],
+            usersInfo: [...this.state.usersInfo, newUserInfo],
+            loggedInUser: id
+        })
     }
 
     addTask = (task) => {
-      const updatedTasks = [...this.state.tasks, task];
-      this.setState({
-        ...this.state,
-        tasks: updatedTasks
-      })
+        const updatedTasks = [...this.state.tasks, task];
+        this.setState({
+            ...this.state,
+            tasks: updatedTasks
+        })
     }
 
     deleteTask = (taskToDelete) => {
-      const updatedTasks = this.state.tasks.filter(task => task.id !== taskToDelete);
-      deleteTaskInDb(taskToDelete);
-      this.setState({
-        ...this.state,
-        tasks: updatedTasks
-      })
+        const updatedTasks = this.state.tasks.filter(task => task.id !== taskToDelete);
+        deleteTaskInDb(taskToDelete);
+        this.setState({
+            ...this.state,
+            tasks: updatedTasks
+        })
     }
 
     onDrop = (e, status) => {
-      e.preventDefault();
-      const taskId = parseInt(e.dataTransfer.getData("id"));
+        e.preventDefault();
+        const taskId = parseInt(e.dataTransfer.getData("id"));
 
-      let tasks = this.state.tasks.filter((task => {
-          if (task.id === taskId){
-              task.status = status;
-          }
-          return task;
-      }));
+        let tasks = this.state.tasks.filter((task => {
+            if (task.id === taskId) {
+                task.status = status;
+            }
+            return task;
+        }));
 
-      updateTaskInDb(taskId, { status });
+        updateTaskInDb(taskId, {status});
 
-      this.setState({
-          ...this.state,
-          tasks
-      })
+        this.setState({
+            ...this.state,
+            tasks
+        })
     }
 
     updateCurrentProject = (e, projectId) => {
-      e.preventDefault();
-      let updatedProjectId = projectId;
-      if (projectId === this.state.currentProject){
-          updatedProjectId = null;
-      }
-      this.setState({
-          ...this.state,
-          currentProject: updatedProjectId
-      })
+        e.preventDefault();
+        let updatedProjectId = projectId;
+        if (projectId === this.state.currentProject) {
+            updatedProjectId = null;
+        }
+        this.setState({
+            ...this.state,
+            currentProject: updatedProjectId
+        })
     }
 
     updateFilterByAssignee = () => {
-      this.setState({
-          ...this.state,
-          showAllUserTasks: !(this.state.showAllUserTasks)
-      })
+        this.setState({
+            ...this.state,
+            showAllUserTasks: !(this.state.showAllUserTasks)
+        })
     }
 
     render() {
-      const contextValue = {
-        loggedInUser: this.state.loggedInUser, 
-        teams: this.state.teams,
-        usersInfo: this.state.usersInfo,
-        usersLogin: this.state.usersLogin, 
-        projects: this.state.projects,
-        tasks: this.state.tasks,
-        currentProject: this.state.currentProject,
-        showAllUserTasks: this.state.showAllUserTasks,
-        onLogInUser: this.logInUser,
-        onSignUpUser: this.signUpUser,
-        onSignOutUser: this.signOutUser,
-        onDrop: this.onDrop,
-        onAddTask: this.addTask,
-        onDeleteTask: this.deleteTask,
-        updateCurrentProject: this.updateCurrentProject,
-        updateFilterByAssignee: this.updateFilterByAssignee
-      }
-      return (
-        <ProductifyContext.Provider value={contextValue}>
-          <div className="app">
-            <Header/>
-            <MainContentRouter/>
-          </div>
-        </ProductifyContext.Provider>
-      );
+        const contextValue = {
+            loggedInUser: this.state.loggedInUser,
+            teams: this.state.teams,
+            usersInfo: this.state.usersInfo,
+            usersLogin: this.state.usersLogin,
+            projects: this.state.projects,
+            tasks: this.state.tasks,
+            currentProject: this.state.currentProject,
+            showAllUserTasks: this.state.showAllUserTasks,
+            onLogInUser: this.logInUser,
+            onSignUpUser: this.signUpUser,
+            onSignOutUser: this.signOutUser,
+            onDrop: this.onDrop,
+            onAddTask: this.addTask,
+            onDeleteTask: this.deleteTask,
+            updateCurrentProject: this.updateCurrentProject,
+            updateFilterByAssignee: this.updateFilterByAssignee
+        }
+        return (
+            <ProductifyContext.Provider value={contextValue}>
+                <div className="app">
+                    <Header/>
+                    <MainContentRouter/>
+                </div>
+            </ProductifyContext.Provider>
+        );
     }
-    
-  }
+
+}

5fb25855afd2bf7ef19c1dcce93055840f27e044
Berk Gedik <berk.gedik@tink.se>
2020-06-04 15:53:35 +0200

    Code suggestion
    Two main changes
    
    1) Assign task-related values (coming from props) into class variables during constructor execution (better practice)
    
    2) Remove parameters for some class methods: since the values of these parameters are now in class variables we don't need any parameter (so these methods are now looking more like a class method rather than a static method)

--- src/Components/TaskCard/TaskCard.js
+++ src/Components/TaskCard/TaskCard.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom';
 import ProductifyContext from '../../ProductifyContext';
 import moment from 'moment';
 import './TaskCard.css';
@@ -8,69 +8,81 @@ export default class TaskCard extends Component {
 
     static contextType = ProductifyContext;
 
+    constructor(props) {
+        super(props);
+        this.id = this.props.task.id;
+        this.task_name = this.props.task.task_name;
+        this.deadline = this.props.task.deadline;
+        this.status = this.props.task.status;
+        this.assignee = this.props.task.assignee;
+    }
+
     onDragStart = (e, taskId) => {
         e.dataTransfer.setData("id", taskId);
     }
 
-    getColor(status){
+    getColor() {
         let color;
-        switch(status) {
+        switch (this.status) {
             case "inProgress":
-                color= 'var(--productify-turquoise)';
+                color = 'var(--productify-turquoise)';
                 break;
             case "inReview":
-                color= 'var(--productify-yellow)';
+                color = 'var(--productify-yellow)';
                 break;
             case "complete":
-                color= 'var(--productify-green)';
+                color = 'var(--productify-green)';
                 break;
             case "backlog":
-                color= 'var(--productify-red)';
+                color = 'var(--productify-red)';
                 break;
             default:
-                color= '#FFF';
-          }
-          return {background: color};
+                color = '#FFF';
+        }
+        return {background: color};
     }
 
-    getDeadline(deadline) {
-        if (deadline !== null){
-            return moment(deadline).format("MMM Do YYYY"); 
+    getDeadline() {
+        if (this.deadline !== null) {
+            return moment(this.deadline).format("MMM Do YYYY");
         }
         return "Deadline not set"
     }
 
-    getInitials = (id) => {
-        const user = this.context.usersInfo.find(user => user.id === id) || {};
+    getInitials() {
+        const user = this.context.usersInfo.find(user => user.id === this.id);
+        if (!user) {
+            console.log("warning : cannot find user with id " + this.id);
+            return "";
+        }
         const initials = user.first_name.charAt(0) + user.last_name.charAt(0);
         return initials;
     }
 
-    getAssigneeAvatar = (id) => {
+    getAssigneeAvatar() {
         return (
             <div className="assigneeAvatarContainer">
                 <h5 className="assignee">Assignee:&nbsp;</h5>
                 <div className='assigneeAvatar'>
-                    <span className="assigneeAvatar__initials">{this.getInitials(id)}</span>
+                    <span className="assigneeAvatar__initials">{this.getInitials()}</span>
                 </div>
             </div>
         )
     }
 
     render() {
-        const { id, task_name, deadline, status, assignee} = this.props.task;
         return (
-            <NavLink to={`/projects/${id}`} className="taskCard__nav">
-                <div key={id} 
-                value={id}
-                draggable 
-                onDragStart = {(e) => this.onDragStart(e, id)}
-                className="taskCard" 
-                title={task_name} 
-                style={this.getColor(status)}>
-                    <h4 className="taskCard__name">{task_name}</h4>
-                    <h5 className="taskCard__deadline">{this.getDeadline(deadline)}</h5>
-                    {assignee && this.getAssigneeAvatar(assignee)}
+            <NavLink to={`/projects/${this.id}`} className="taskCard__nav">
+                <div key={this.id}
+                     value={this.id}
+                     draggable
+                     onDragStart={(e) => this.onDragStart(e, this.id)}
+                     className="taskCard"
+                     title={this.task_name}
+                     style={this.getColor()}>
+                    <h4 className="taskCard__name">{this.task_name}</h4>
+                    <h5 className="taskCard__deadline">{this.getDeadline()}</h5>
+                    {this.assignee && this.getAssigneeAvatar()}
                 </div>
             </NavLink>
         )
